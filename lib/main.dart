053b75e1083e31288d142453f0bdc7c8523024fb
副島優希 <soejimayuuki@soejimaukinoAir.elecom>
2024-02-10 14:49:53 +0900

    first-flutter

---
+++ lib/main.dart
@@ -0,0 +1,78 @@
+import 'package:flutter/material.dart';
+import 'next_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      // This call to setState tells the Flutter framework that something has
+      // changed in this State, which causes it to rerun the build method below
+      // so that the display can reflect the updated values. If we changed
+      // _counter without calling setState(), then the build method would not be
+      // called again, and so nothing would appear to happen.
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme
+            .inversePrimary,
+        title: Text('Flutter大学'),
+        bottom: PreferredSize(
+            child: Container(
+              height: 5, //高さ
+              color: Colors.red, //色
+            ),
+            preferredSize: Size.fromHeight(5)),//高さ
+      ),
+      body: Container(
+        width: double.infinity,
+        child: Column(
+          children: [
+            TextField(),
+            TextField(),
+          ],
+        ),
+
+
+      ),
+    );
+  }
+}
+
+
